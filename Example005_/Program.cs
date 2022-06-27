944a82e83a9e38b04c5b1d54413847ac08148cbd
Maksim <bagzmax75@gmail.com>
2022-06-28 01:00:49 +0300

    add Example 005

---
+++ Example005_/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задано два числа, определить если кратно 2 к 1, то выводим сообщение, если нет, то показываем остаток от деления.
+
+Console.Write("Введите первое число: ");
+int numberone = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numbertwo = int.Parse(Console.ReadLine());
+
+if (numberone % numbertwo == 0)
+{
+   System.Console.WriteLine($"{numberone} кратно {numbertwo}"); 
+}
+else
+{
+    System.Console.WriteLine(
+        $"{numberone} не кратно {numbertwo}" + 
+        $" остаток {numberone % numbertwo}"
+        );
+}
