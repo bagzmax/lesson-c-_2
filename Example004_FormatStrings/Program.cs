f97cdc04e1021cadd4194044175215e426e67c58
Maksim <bagzmax75@gmail.com>
2022-06-28 00:33:06 +0300

    add Example 004 Format

---
+++ Example004_FormatStrings/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 5, b = 4, c = 10;
+
+//System.Console.WriteLine("Число: " + a);
+
+//System.Console.WriteLine($"Число: {a}"); // $ - форматирование строк
+
+System.Console.WriteLine("Число: {2}, {1}, {2}", a, b, c);
